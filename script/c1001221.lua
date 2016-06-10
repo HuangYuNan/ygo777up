98b65fcc450bfe0b1728e481a3486ff7479562f8
HuangYuNan <luwmxach@qq.com>
2016-06-10 13:03:33 +0800

    0610fixv1

--- script/c1001221.lua
+++
@@ -1,9 +0,0 @@
---朝潮级驱逐舰1号舰—朝潮
-require "script/c1001252"
-function c1001221.initial_effect(c)
-	colle.sum(c,1)
-	colle.atkup(c,100)
-	colle.cnb(c)
-	colle.th(c)
-	colle.defwd(c)
-end
--- script/c1001227.lua
+++
@@ -1,9 +0,0 @@
---初春级驱逐舰1号舰—初春
-require "script/c1001252"
-function c1001227.initial_effect(c)
-	colle.sum(c,1)
-	colle.atkup(c,100)
-	colle.cnb(c)
-	colle.th(c)
-	colle.defwd(c)
-end
