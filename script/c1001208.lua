49b540d4750edfdc0b628d38f5d1adc5d2beccf5
HuangYuNan <119724848@qq.com>
2016-06-12 08:46:59 +0800

    0612fixv5

--- script/c1001208.lua
+++ script/c1001208.lua
@@ -78,9 +78,9 @@ function c1001208.filter_b(c)
 end
 function c1001208.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_ONFIELD) and c1001208.filter_a(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c1001208.filter_a,tp,LOCATION_ONFIELD,0,1,e:GetHandler()) and Duel.IsExistingTarget(c1001208.filter_b,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(c1001208.filter_a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) and Duel.IsExistingTarget(c1001208.filter_b,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c1001208.filter_a,tp,LOCATION_ONFIELD,0,1,1,e:GetHandler())
+	local g=Duel.SelectTarget(tp,c1001208.filter_a,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c1001208.activate(e,tp,eg,ep,ev,re,r,rp)
